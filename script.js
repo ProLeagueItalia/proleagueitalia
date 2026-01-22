69232994d178a8cb8e496645f007c0f0f2518272
ProLeagueItalia <proleagueitalia.eafc@gmail.com>
2026-01-22 14:43:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-console.log('Pro League Italia loaded');
-document.addEventListener("DOMContentLoaded", function() {
-  document.querySelectorAll('.squadra img').forEach(function(img) {
-    img.onerror = function() { this.style.display = 'none'; };
-  });
-});
